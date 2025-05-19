5bc20c515ef16693fcd7a9f17dce0874e8f07f65
Dmitry <dsanany1@mts.ru>
2025-05-19 10:39:31 +0300

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 import api
 
@@ -8,6 +9,15 @@ app = FastAPI(
     docs_url="/tls_test/docs",
     openapi_url="/tls_test/docs_json",
     default_response_class=ORJSONResponse,
+)
+
+# Добавляем настройки CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://truepotatoes.ru", "https://confluence.mts.ru"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api.router, prefix="/tls_test/api/v1", tags=["Test"])
